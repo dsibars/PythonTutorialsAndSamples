433a1c3a1866eb10b3f6e1bb1ac205e8f593592c
alvarorodriguez <alvarorg05@gmail.com>
2019-05-11 19:13:04 +0200

    he volgut fer un oigas. TEST

--- labs/RODRI/TestWithClass.py
+++ labs/RODRI/TestWithClass.py
@@ -2,7 +2,28 @@
 
 # Create a class with functionality
 
-class MyPrinter(object):
-    def __init__(self, text, number):
-        self.text = text
-        self.number = number
+
+class Friend(object):
+    def __init__(self, name, friend_of, friend_level):
+        self.name = name
+        self.friend_of = friend_of
+        self.friend_level = friend_level
+
+    def salutate(self):
+        print('hello my friend' + ', ' + self.friend_of + '. How are you?')
+
+    def present_yourself(self):
+        print('hello! I am ' + self.name)
+
+    def say_all(self):
+        self.present_yourself()
+        self.salutate()
+
+
+
+dani = Friend('Dani', 'Rodri', 10)
+
+dani.present_yourself()
+dani.salutate()
+
+
