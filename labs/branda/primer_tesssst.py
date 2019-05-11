37fa96c24c8ca05cf0a394e2ea2308ec8ed9795e
Gimihub <guillembrandariz@gmail.com>
2019-05-11 17:58:59 +0200

    mes clenxes

--- labs/branda/primer_tesssst.py
+++ labs/branda/primer_tesssst.py
@@ -18,6 +18,13 @@ clenxitext = str(clenxes_matinals)
 
 print(material_sa + ': ' + clenxitext)
 
+def build_with_number(text, number):
+    return text + str(number)
+
+sumada = build_with_number(material_clenxil, clenxes_matinals)
+
+print(sumada)
+
 def print_with_number(text, number):
     print(text + str(number))
 
