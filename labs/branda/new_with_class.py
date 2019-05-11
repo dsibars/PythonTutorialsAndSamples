afd742f59648828f5a6f7182b7ca790e7baa3fa9
Gimihub <guillembrandariz@gmail.com>
2019-05-11 19:08:15 +0200

    mes clenxes

--- labs/branda/new_with_class.py
+++ labs/branda/new_with_class.py
@@ -6,12 +6,22 @@ class Friend(object):
         self.friend_level = friend_level
 
     def salute(self):
-        print('hello my friend ' + self.friend_of + '. How are you?')
+        print('Hello my friend ' + self.friend_of + '. How are you?')
 
     def introduce_yourself(self):
         print('Hello! I am ' + self.name)
+
+    def say_all(self):
+        self.introduce_yourself()
+        self.salute()
+
 
 Rodri = Friend('Rodri', 'Dani', '10')
 
 Rodri.salute()
 Rodri.introduce_yourself()
+
+Dani = Friend('Dani', 'Rodri', '10')
+Dani.salute()
+
+Dani.say_all()
