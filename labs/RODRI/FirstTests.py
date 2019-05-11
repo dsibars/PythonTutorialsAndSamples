85cbc860529b2951451928a86eb67054ac8e6b20
alvarorodriguez <alvarorg05@gmail.com>
2019-05-11 17:41:00 +0200

    he volgut fer un oigas. TEST

--- labs/RODRI/FirstTests.py
+++ labs/RODRI/FirstTests.py
@@ -1,5 +1,6 @@
 #print('oigas')
 aparatu_gaes = 'oigas'
+caca = 'cera'
 print(aparatu_gaes)
 
 Nivell_Sordesa = 1
@@ -7,3 +8,8 @@ Nivell_Sordesa2 = 2
 Nivell_Sordesa3 = 3
 Nivell_Sordesa4 = 4
 Nivell_Sordesa5 = 5
+
+print(Nivell_Sordesa + Nivell_Sordesa3)
+
+print(aparatu_gaes + caca)
+print(caca + str(Nivell_Sordesa3))
