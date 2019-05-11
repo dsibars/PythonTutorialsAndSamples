2d2414656847b73c1df998f985e21ae1535a076b
dsepulveda <dsepulveda@redpoints.com>
2019-05-11 17:50:40 +0200

    initial version

--- labs/dani/FirstTests.py
+++ labs/dani/FirstTests.py
@@ -1,2 +1,23 @@
-print('heey')
+patata = 'something'
+cebolla = ' else'
+
+print(patata)
+
+numerito1 = 69
+numerito2 = 133
+
+numerito1entexto = str(numerito1)
+print('el numerito en texto es: ' + numerito1entexto)
+
+
+print(numerito1 + numerito2)
+print(patata + cebolla)
+print(patata + str(numerito1))
+
+
+def print_with_number(text, number):
+    print(text + str(number))
+
+
+print_with_number('hola ', 69)
 
