bb1325047fff86758804422ea296284cebbbfbf5
Gimihub <guillembrandariz@gmail.com>
2019-05-11 17:40:36 +0200

    mes clenxes

--- labs/branda/primer_tesssst.py
+++ labs/branda/primer_tesssst.py
@@ -1,4 +1,15 @@
 # print('coca')
 # print('clenxes FC')
+
 material_clenxil = 'keta'
-print(material_clenxil)
+material_sa = 'tofu'
+
+#print(material_clenxil)
+
+clenxes_matinals = 2
+clenxes_nocturnes = 6
+
+print(clenxes_matinals + clenxes_nocturnes)
+print(material_clenxil + material_sa)
+
+print(material_sa + ': ' + str(clenxes_matinals))
