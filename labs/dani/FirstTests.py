6a7eccd4bdc4aac6d62e77f93c8828193a896ebf
dsepulveda <dsepulveda@redpoints.com>
2019-05-11 18:19:12 +0200

    initial version

--- labs/dani/FirstTests.py
+++ labs/dani/FirstTests.py
@@ -4,7 +4,9 @@ cebolla = ' else'
 print(patata)
 
 numerito1 = 69
-numerito2 = 133
+numerito2 = 13
+numerito1 = 73
+
 
 numerito1entexto = str(numerito1)
 print('el numerito en texto es: ' + numerito1entexto)
@@ -15,8 +17,19 @@ print(patata + cebolla)
 print(patata + str(numerito1))
 
 
+def build_with_number(text, number):
+    return text + str(number)
+
+
+resultat = build_with_number(patata, numerito2)
+
+print(resultat)
+
+
 def print_with_number(text, number):
-    print(text + str(number))
+    text_to_print = build_with_number(text, number)
+    print(text_to_print)
+
 
 
 print_with_number('hola ', 69)
