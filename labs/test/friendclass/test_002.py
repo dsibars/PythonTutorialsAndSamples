8863c63fd3f1810aa59f6a3c7f6eed518fe532a4
dsepulveda <dsepulveda@redpoints.com>
2019-05-16 20:10:54 +0200

    first exercise

--- labs/test/friendclass/test_002.py
+++ labs/test/friendclass/test_002.py
@@ -14,27 +14,19 @@ class TestFriend002(unittest.TestCase):
         self.target = Friend(self.test_name, self.test_friendof, self.test_level)
 
     def test_change_name(self):
-        # Test friend level to low value
-        test_new_level = 8
-
-        # Set a new level
-        self.target.set_level(test_new_level)
-
+        # Test change name functionality
+        test_new_name = 'petete'
+        # Set a new name
+        self.target.set_name(test_new_name)
         # Expected result
-        expected_result = 'OH MY GOD I LOVE YOU!'
-
+        expected_result = 'hello! I am ' + test_new_name
         # Generate the result
-        result1 = self.target.present_yourself()
-        result2 = self.target.salutate()
-        result3 = self.target.say_all()
-
-        # Restore the level
-        self.target.set_level(self.test_level)
+        result = self.target.present_yourself()
+        # Restore the name
+        self.target.set_name(self.test_name)
 
         # Check if the expected result and result are the same
-        self.assertEquals(result1, expected_result)
-        self.assertEquals(result2, expected_result)
-        self.assertEquals(result3, expected_result)
+        self.assertEquals(result, expected_result)
 
 
 if __name__ == '__main__':
