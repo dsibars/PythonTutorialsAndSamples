5e50ff720041d3ee8b76bcb66f8ec36ec62db602
Gimihub <guillembrandariz@gmail.com>
2019-05-21 17:08:07 +0200

    clenx me to the moon

--- labs/Friend.py
+++ labs/Friend.py
@@ -1,18 +1,22 @@
+import sys
+
+
 class Friend(object):
     def __init__(self, name, friend_of, friend_level):
         self.name = name
         self.friend_of = friend_of
         self.friend_level = friend_level
 
-    def set_level(self, newlevel):
-        self.friend_level = newlevel
 
-    if self.friend_level >= 8:
-        print('OH MY GOD I LOVE YOU!')
-        return  #talla el rollo ( no se com es diu aquesta funcio)
-    if self.friend_level <= 3:
-        print('Fuck you!')
-        return #talla el rollo ( no se com es diu aquesta funcio)
+#    def set_level(self, newlevel):
+#        self.friend_level = newlevel
+#
+#    if self.friend_level >= 8:
+#        print('OH MY GOD I LOVE YOU!')
+#        sys.exit() #talla el rollo ( no se com es diu aquesta funcio)
+#    if self.friend_level <= 3:
+#        print('Fuck you!')
+#        sys.exit() #talla el rollo ( no se com es diu aquesta funcio)
 
 
     def set_name(self, newname):
